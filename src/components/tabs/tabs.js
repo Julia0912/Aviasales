81c434d2155bd4ca179139a26d37a3bf8da6cd91
Julia0912 <lulenika@icloud.com>
2024-02-08 16:41:35 +0400

    fix checkboxes and tabs

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -2,29 +2,40 @@ import React from "react";
 import css from "./tabs.module.scss";
 import { connect } from "react-redux";
 import * as actions from "../../redux/actions";
+import cn from "classnames";
 const Tabs = ({ state, tabs }) => {
+  // const liClasses = ["button", "button_one", "two", "three"];
   return (
     <div className={css.filter}>
       <button
-        style={{ background: state.buttons[0] && "#2196F3" }}
+        style={{
+          background: state.buttons[0] && "#2196F3",
+          color: state.buttons[0] && "#FFF",
+        }}
         id={"0"}
-        className={css.button}
+        className={cn(css.button, css.button_one)}
         onClick={(event) => tabs(event.target.id)}
       >
         Самый дешевый
       </button>
       <button
-        style={{ background: state.buttons[1] && "#2196F3" }}
+        style={{
+          background: state.buttons[1] && "#2196F3",
+          color: state.buttons[1] && "#FFF",
+        }}
         id={"1"}
-        className={css.button}
+        className={cn(css.button, css.button_two)}
         onClick={(event) => tabs(event.target.id)}
       >
         Самый быстрый
       </button>
       <button
-        style={{ background: state.buttons[2] && "#2196F3" }}
+        style={{
+          background: state.buttons[2] && "#2196F3",
+          color: state.buttons[2] && "#FFF",
+        }}
         id={"2"}
-        className={css.button}
+        className={cn(css.button, css.button_three)}
         onClick={(event) => tabs(event.target.id)}
       >
         Оптимальный
--- src/components/transfers-filter/transfers-filter.js
+++ src/components/transfers-filter/transfers-filter.js
@@ -2,59 +2,67 @@ import React from "react";
 import css from "./transfers-filter.module.scss";
 import { connect } from "react-redux";
 import * as actions from "../../redux/actions";
+import cn from "classnames";
 
 const TransfersFilter = ({ state, checkBoxClick }) => {
   return (
     <div className={css.transfers}>
       <p> {"Количество пересадок"}</p>
-      <ul>
-        <li className={css.li}>
-          <input
-            id={"0"}
-            onChange={(event) => checkBoxClick(event.target.id)}
-            type="checkbox"
-            checked={state?.checkBox[0]}
-          />
-          Все
-        </li>
 
-        <li className={css.li}>
-          <input
-            id={"1"}
-            onChange={(event) => checkBoxClick(event.target.id)}
-            checked={state.checkBox[1]}
-            type="checkbox"
-          />
-          Без пересадок
-        </li>
-        <li className={css.li}>
-          <input
-            id={"2"}
-            type="checkbox"
-            onChange={(event) => checkBoxClick(event.target.id)}
-            checked={state.checkBox[2]}
-          />
-          1 пересадка
-        </li>
-        <li className={css.li}>
-          <input
-            id={"3"}
-            type="checkbox"
-            onChange={(event) => checkBoxClick(event.target.id)}
-            checked={state.checkBox[3]}
-          />
-          2 пересадки
-        </li>
-        <li className={css.li}>
-          <input
-            id={"4"}
-            type="checkbox"
-            onChange={(event) => checkBoxClick(event.target.id)}
-            checked={state.checkBox[4]}
-          />
-          3 пересадки
-        </li>
-      </ul>
+      <label className={cn(css.check)}>
+        <input
+          className={cn(css.checkBox)}
+          id={"0"}
+          onChange={(event) => checkBoxClick(event.target.id)}
+          type="checkbox"
+          checked={state?.checkBox[0]}
+        />
+        <span className={cn(css.checkBoxSpan)}></span>
+        Все
+      </label>
+
+      <label className={cn(css.check)}>
+        <input
+          className={cn(css.checkBox)}
+          id={"1"}
+          onChange={(event) => checkBoxClick(event.target.id)}
+          checked={state.checkBox[1]}
+          type="checkbox"
+        />
+        <span className={cn(css.checkBoxSpan)}></span>
+        Без пересадок
+      </label>
+
+      <label className={cn(css.check)}>
+        <input
+          className={cn(css.checkBox)}
+          id={"2"}
+          type="checkbox"
+          onChange={(event) => checkBoxClick(event.target.id)}
+          checked={state.checkBox[2]}
+        />
+        <span className={cn(css.checkBoxSpan)}></span>1 пересадка
+      </label>
+      <label className={cn(css.check)}>
+        <input
+          className={cn(css.checkBox)}
+          id={"3"}
+          type="checkbox"
+          onChange={(event) => checkBoxClick(event.target.id)}
+          checked={state.checkBox[3]}
+        />
+        <span className={cn(css.checkBoxSpan)}></span>2 пересадки
+      </label>
+      <label className={cn(css.check)}>
+        <input
+          className={cn(css.checkBox)}
+          id={"4"}
+          type="checkbox"
+          onChange={(event) => checkBoxClick(event.target.id)}
+          checked={state.checkBox[4]}
+        />
+        <span className={cn(css.checkBoxSpan)}></span>3 пересадки
+      </label>
     </div>
   );
 };
